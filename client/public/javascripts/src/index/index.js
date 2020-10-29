b311c7784ef5c2238e3fdafd57a6791f8eb18d38
zhoujin <171226Q2>
2020-10-29 14:52:34 +0800

    html热更新

--- client/public/javascripts/src/index/index.js
+++ client/public/javascripts/src/index/index.js
@@ -1,6 +1,20 @@
+import axios from 'axios'
 import 'less/index.less'
-console.log('index!!!!!')
+console.log('index')
+const isDev = process.env.NODE_ENV === 'development'
 
+if(isDev){
+    require('raw-loader!../../../../../dev-views/views/index.html')
+}
 if(module.hot){
     module.hot.accept()
+    module.hot.dispose(() => {
+		const href = window.location.href
+		axios.get(href).then(res => {
+			const template = res.data
+			document.body.innerHTML = template
+		}).catch(e => {
+			console.error(e)
+		})
+	})
 }
